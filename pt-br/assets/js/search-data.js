10a75f0d2c84baf8c5887bbf364145e8ae8cd18b
mlabussiere <95632080+mlabussiere@users.noreply.github.com>
2025-07-30 08:04:18 +0000

    Deploying to gh-pages from @ mlabussiere/mlabussiere.github.io@f86a2a367c145a11ddb32a880489614e1d4fce09 🚀

--- pt-br/assets/js/search-data.js
+++ pt-br/assets/js/search-data.js
@@ -519,35 +519,42 @@ ninja.data = [{
         title: 'Enviar um email',
         section: 'Redes sociais',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D.%6C%61%62%75%73%73%69%65%72%65@%75%76%61.%6E%6C", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Redes sociais',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Redes sociais',
-        handler: () => {
-          window.open("./feed.xml", "_blank");
+          window.open("https://orcid.org/0000-0003-4115-7883", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Redes sociais',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=3yoTbEUAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Redes sociais',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://github.com/mlabussiere", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Redes sociais',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/marie-labussiere-", "_blank");
+        },
+      },{
+        id: 'social-bluesky',
+        title: 'Bluesky',
+        section: 'Redes sociais',
+        handler: () => {
+          window.open("https://bsky.app/profile/labussieremarie.bsky.social", "_blank");
         },
       },{
           id: 'lang-en-us',
